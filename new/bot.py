5e9ccb19cc20655ab6a8ee56380044f28e296747
Baumkobra <dasobjektiv700@gmail.com>
2022-02-16 21:13:32 +0100

    1

--- new/bot.py
+++ new/bot.py
@@ -1,8 +1,21 @@
-import discord as dc
+import discord 
 
 
+client = discord.Client()
 
-class Client:
-  def __init__(self) -> None:
-    
-    
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="Admin Rechte usen"))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        await message.delete()
+
+@client.event
+async def on_message(message):
+   pass
+
+@client.event
+async def on_reaction_add(reaction, user):
+  pass
